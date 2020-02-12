751237b09ebdc6cb75444eb22446fe3f46f403af
davidaskenazy <davidask.da@gmail.com>
2020-02-12 11:17:29 -0300

    configurando notificaciones push mediante .fetch js

--- src/modules/Push/containers/push.js
+++ src/modules/Push/containers/push.js
@@ -1,40 +1,67 @@
-import React, {Component} from 'react'
-import {View,Text, StyleSheet} from 'react-native'
-import {Notifications} from 'expo'
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { Notifications } from 'expo'
 import * as Permissions from 'expo-permissions'
 
-class Push extends Component{
-    
-    async componentDidMount(){
-        const {status: existingStatus} = await Permissions.getAsync(
+let miToken = ""
+
+class Push extends Component {
+
+    async componentDidMount() {
+        const { status: existingStatus } = await Permissions.getAsync(
             Permissions.NOTIFICATIONS
         );
         let finalStatus = existingStatus;
 
-        if(existingStatus !== 'granted'){
-            const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        if (existingStatus !== 'granted') {
+            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
             finalStatus = status;
         }
-        if(finalStatus !== 'granted'){
+        if (finalStatus !== 'granted') {
             return;
         }
 
         let token = await Notifications.getExpoPushTokenAsync();
-        console.log(token)
+        miToken = token;
     }
-    
-    render(){
-        return(
+
+    sendNotif() {
+        fetch('https://exp.host/--/api/v2/push/send', {
+            method: 'POST',
+            mode: 'no-cors',
+            credentials: 'same-origin',
+            headers: {
+                'Host': 'exp.host',
+                'Accept': 'application/json',
+                'Accept-encoding': 'gzip, deflate',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                to: miToken,
+                title: "title",
+                body: "mensaje"
+
+            })
+        }).then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson)
+            }).catch((error) => {
+                console.error(error);
+            });
+    }
+
+    render() {
+        return (
             <View style={styles.container}>
-                <Text> Push </Text>
+                <Text onPress={()=>this.sendNotif()}> Push </Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     }
